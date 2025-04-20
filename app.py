8728af5612228f115b1b3bd993fa72c5e20781d3
ayush-jain-sparsh <151443527+ayush-jain-sparsh@users.noreply.github.com>
2025-04-20 11:46:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def index():
     return "Welcome to the SkillSetu! to access our service click on the link -> "
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000,debug=True)
