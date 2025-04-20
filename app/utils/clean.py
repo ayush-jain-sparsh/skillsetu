f635f72ee80e0e2ed13bea8d3cccda6e6bb245de
ayush-jain-sparsh <151443527+ayush-jain-sparsh@users.noreply.github.com>
2025-04-20 12:49:39 +0530

    Update clean.py

--- app/utils/clean.py
+++ app/utils/clean.py
@@ -1,6 +1,6 @@
 import json
 def clean_json(output):
     cleaned_json_string = output.replace("```json", "").replace("```", "").strip()
-    cleaned_json_string = cleaned_json_string.replace("\n","<br>)
+    cleaned_json_string = cleaned_json_string.replace("\n","<br>")
     json_data = json.loads(cleaned_json_string)
     return json_data
