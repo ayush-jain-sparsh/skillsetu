73f0585a58eaf8dd823d6347cc8aba419ad13294
ayush-jain-sparsh <151443527+ayush-jain-sparsh@users.noreply.github.com>
2025-04-20 12:04:12 +0530

    Update chatbot.py

--- LangChain/prompts/chatbot.py
+++ LangChain/prompts/chatbot.py
@@ -1,5 +1,5 @@
 
-def get_mentor():
+def get_chat():
     text = """You are a just bot which will solve query on the basis of past event and you also have to summarise the given prompt as much concise as
     you can for future history use and don't have to reply about history just concate the topic with history for fine answer. and don't use ** or # 
     for bold or anything like that 
